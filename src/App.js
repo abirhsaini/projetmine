183b10a95e3595b335de34436815ec1b153052fc
abirhsaini <abirhsaini@gmail.com>
2022-03-15 19:40:46 +0000

    p

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Reservation from "./pages/reservation";
+import Menu from "./pages/Menu";
+import Break from "./pages/Break";
+import Lunch from "./pages/lunch";
+import Dinner from "./pages/Dinner";
+import Contact from "./pages/Contact";
+import Order from "./pages/Order";
+
+import { CartProvider} from "react-use-cart";
+
+
+import {BrowserRouter ,Switch ,Route} from "react-router-dom";
+import "./style/tout.scss"
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+  <div className="brows">
+    <CartProvider>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/menu" exact component={Menu} />
+      <Route path="/reservation" exact component={Reservation} />
+      <Route path="/about" exact component={About} />
+      
+      <Route path="/lunch" exact component={Lunch} />
+     < Route path="/breakfast" exact component={Break} />
+      <Route path="/dinner" exact component={Dinner} />
+      <Route path="/Contact" exact component={Contact} />
+      <Route path="/order" exact component={Order} />
+     
+      
+    </Switch>
+    </BrowserRouter></CartProvider></div>
   );
 }
 
 export default App;
+
+
+
